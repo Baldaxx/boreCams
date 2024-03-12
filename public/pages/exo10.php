21c2a5857399971e58b90cabf602dada87d1fd5e
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-03-12 08:44:49 +0100

    exercice 10

--- public/pages/exo10.php
+++ public/pages/exo10.php
@@ -2,7 +2,20 @@
 include '../common/head.php';
 include '../common/nav.php';
 ?>
+<h2>Personnage :</h2>
 
-<?php
-include '../common/footer.php';
-?>
+    <?php
+    echo '<br><img src="./img/player.png" alt="player Luke" /><br>
+Nom : Luke<br>Age : 27<br>Sexe : Homme<br>Force : 5<br>Agilité : 4';
+
+    echo '<br><img src="./img/playerF.png" alt="player Katy" /><br>
+Nom : Katy<br>Age : 22<br>Sexe : Femme<br>Force : 3<br>Agilité : 6';
+
+    echo '<br><img src="./img/playerM.png" alt="player Marc" /><br>
+Nom : Marc<br>Age : 33<br>Sexe : Homme<br>Force : 7<br>Agilité : 2';
+
+    echo '<br></div>';
+    ?>
+    <?php
+    include '../common/footer.php';
+    ?>
