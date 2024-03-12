90a70179d164107d8192de9f5c6e2a8547e3a130
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-03-12 08:40:08 +0100

    exercice 8-9

--- public/pages/exo8.php
+++ public/pages/exo8.php
@@ -3,6 +3,40 @@ include '../common/head.php';
 include '../common/nav.php';
 ?>
 
-<?php
-include '../common/footer.php';
-?>
+<h1>Sélection du personnage</h1>
+<form action="#" method="POST">
+    <label for="perso">Personnage : </label>
+    <select name="perso" id="perso">
+        <option value="p1">Luke</option>
+        <option value="p2">Katy</option>
+        <option value="p3">Marc</option>
+    </select> <button type="submit">Voir le personnage</button>
+</form>
+
+<h2>Personnage :</h2>
+<div class='gauche'>
+
+    <?php
+    if (isset($_POST['perso'])) {
+        $personnage = $_POST['perso'];
+        switch ($personnage) {
+            case "p1":
+                echo '<img src="./img/player.png" alt="player Luke" /><br>
+Nom : Luke<br>Age : 27<br>Sexe : Homme<br>Force : 5<br>Agilité : 4';
+                break;
+            case "p2":
+                echo '<img src="./img/playerF.png" alt="player Katy" /><br>
+Nom : Katy<br>Age : 22<br>Sexe : Femme<br>Force : 3<br>Agilité : 6';
+                break;
+            case "p3":
+                echo '<img src="./img/playerM.png" alt="player Marc" /><br>
+Nom : Marc<br>Age : 33<br>Sexe : Homme<br>Force : 7<br>Agilité : 2';
+                break;
+        }
+        echo '</div>';
+    }
+    ?>
+
+    <?php
+    include '../common/footer.php';
+    ?>
--- public/pages/exo9.php
+++ public/pages/exo9.php
@@ -2,7 +2,58 @@
 include '../common/head.php';
 include '../common/nav.php';
 ?>
+<h1>Sélection du personnage</h1>
+<form action="#" method="POST">
+    <label for="perso">Personnage : </label>
+    <select name="perso" id="perso">
+        <option value="p1">Luke</option>
+        <option value="p2">Katy</option>
+        <option value="p3">Marc</option>
+    </select>
+    <button type="submit">Voir le personnage</button>
+</form>
 
+<h2>Personnage :</h2>
+<div class='gauche'>
+    
+    <?php
+    $personnages = [
+        "p1" => [
+            "image" => "./img/player.png",
+            "nom" => "Luke",
+            "age" => 27,
+            "sexe" => "Homme",
+            "force" => 5,
+            "agilite" => 4,
+        ],
+        "p2" => [
+            "image" => "./img/playerF.png",
+            "nom" => "Katy",
+            "age" => 22,
+            "sexe" => "Femme",
+            "force" => 3,
+            "agilite" => 6,
+        ],
+        "p3" => [
+            "image" => "./img/playerM.png",
+            "nom" => "Marc",
+            "age" => 33,
+            "sexe" => "Homme",
+            "force" => 7,
+            "agilite" => 2,
+        ],
+    ];
+
+    if (isset($_POST['perso'])) {
+        $personnage = $_POST['perso'];
+        if (array_key_exists($personnage, $personnages)) {
+            $p = $personnages[$personnage];
+            echo "<img src='{$p["image"]}' alt='player {$p["nom"]}' /><br>
+Nom : {$p["nom"]}<br>Age : {$p["age"]}<br>Sexe : {$p["sexe"]}<br>Force : {$p["force"]}<br>Agilité : {$p["agilite"]}";
+        }
+    }
+    ?>
+</div>
 <?php
 include '../common/footer.php';
 ?>
